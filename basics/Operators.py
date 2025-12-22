310a426c4115f786e274c2a2c6f18a277b2c52b7
Jay Pradeep Ukarde <jayukarde10@gmail.com>
2025-12-23 02:23:53 +0530

    operators in python

---
+++ basics/Operators.py
@@ -0,0 +1,48 @@
+# Operator	Name	Example	
+# +	Addition	x + y	
+# -	Subtraction	x - y	
+# *	Multiplication	x * y	
+# /	Division	x / y	
+# %	Modulus	x % y	
+# **	Exponentiation	x ** y	
+# //	Floor division	x // y
+
+a=int(input("enter a value"))
+b=int(input("enter b value"))
+
+print("Addition:",a+b)
+print("Subtraction:",a-b)
+print("Multiplication:",a*b)
+print("Division:",a/b)
+print("Modulus:",a%b)
+print("Exponentiation:",a**b)
+print("Floor Division:",a//b)
+
+# Comparison Operators
+# Operator	Name	Example
+# ==	Equal	x == y
+# !=	Not equal	x != y      
+# >	Greater than	x > y
+# <	Less than	x < y
+# >=	Greater than or equal to	x >= y
+# <=	Less than or equal to	x <= y
+
+print("Equal:",a==b)# give True if a is equal to b else False
+
+
+# Operator	Example	Same As	Try it
+# =	x = 5	x = 5	
+# +=	x += 3	x = x + 3	
+# -=	x -= 3	x = x - 3	
+# *=	x *= 3	x = x * 3	
+# /=	x /= 3	x = x / 3	
+# %=	x %= 3	x = x % 3	
+# //=	x //= 3	x = x // 3	
+# **=	x **= 3	x = x ** 3	
+# &=	x &= 3	x = x & 3	.... bit wise ,0-1,and
+# |=	x |= 3	x = x | 3	
+# ^=	x ^= 3	x = x ^ 3	
+# >>=	x >>= 3	x = x >> 3	
+# <<=	x <<= 3	x = x << 3	
+# :=	print(x := 3)	x = 3  ... it assigh under print and return value
+#                       print(x)
