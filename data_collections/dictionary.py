5164c1c0d87178be745af086bca07625fa97eefb
Jay Pradeep Ukarde <jayukarde10@gmail.com>
2026-02-19 22:42:26 +0530

    dictonary file is updated

--- data_collections/dictionary.py
+++ data_collections/dictionary.py
@@ -10,8 +10,8 @@ dict={"name":"jay",
       "city":"delhi",
       "subjects":["maths","sci"], # list in dict
       "tup":(1,2,2),
-       (1,2,3):"tup" }# only tuple becuse it is immutable not changeble , list & dict are changeble
-
+       (1,2,3):"tup" }# only tuple becuse it is immutable not changeble , list  are changeble
+                     # dict can use as nested dict also
 
 print(dict)
 print(dict["name"])
@@ -26,7 +26,33 @@ print(dict)
 
 null_dict={} #empty dict
 
+#nested dict
+student={
+    "key":"value",
+    "name":"jay",
+    "age":22,
+    "subjects":{
+        "maths":90,
+        "sci":80
+    }
+}
+
+print(student["subjects"]["sci"])
+
+
+#change in nested dict
+student["subjects"]["sci"]=90
+print(student["subjects"])
+
+#methods
+print("methods")
+print(student.keys()) #return all keys 
+print(len(student)) #return length 
+print(student.values())#return all values
+print(student.items())#return (key, val) pair
+print(student.get("key"))# returns the key according to value
+student.update({"city":"mumbai","dist":"maharashtra"})
+print(student)## update and insert in dict
 
 
 
- 
