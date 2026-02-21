b8c11e8356935a2833845fea2da5b7ef9781aed0
Jay Pradeep Ukarde <jayukarde10@gmail.com>
2026-02-21 19:30:16 +0530

    for loop

--- loops.py/for loop.py
+++ loops.py/for loop.py
@@ -1,5 +1,13 @@
+#syntax
+# for i in (list/tuple/any):
+#     #code like if else
+#     #increment or decrement
+# else:
+#     #work when loop end ,not get executed in break
 
-
+#for i in range(start,stop,step) # step mean how much get incrase ex if (0,10,2) it increment by 2 ,+=2
+# for i in range(10,0,-1):#10 to 1
+    
 
 # l=[1,2,3,4,5,6]
 # # t=tuple(l)
@@ -17,12 +25,25 @@
 # for i in j:
 #     print(i) 
 
-n=16
-for i in range(0,11):
-    j=i*i
-    if(j==n):
-        print("n found",n)
-    else:
-        print(j)
-        
+#range
+
+# n=16
+# for i in range(0,11):
+#     j=i*i
+#     if(j==n):
+#         print("n found",n)
+#     else:
+#         print(j)
+
+# for i in range(0,10,2):
+#     print(i)
+
+
+##reverse imp run once again
+# for i in range(100,0,-1):
+#     print(i)
+
+# n=6
+# for i in range(0,11):
+#     print(i*n)
 
