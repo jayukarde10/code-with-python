ddd47a85a95715b12e103bd32ba1d282b8b612da
Jay Pradeep Ukarde <jayukarde10@gmail.com>
2026-03-24 23:30:09 +0530

    add function to reverse a number

--- projects/rwverse.py
+++ projects/rwverse.py
@@ -9,7 +9,19 @@ print(n[::-1])
 
 #sliceslicing
 l=[]
-for i in range(len(n)):
+for i in range(len(n)-1,-1,-1):
     l.append(n[i])
 print(l)
+
+#
+n = int(input("Enter a number: "))
+
+rev = 0
+
+while n > 0:
+    digit = n % 10
+    rev = rev * 10 + digit
+    n = n // 10
+
+print("Reversed number:", rev)
 
